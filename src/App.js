7aa742757aee50a3c6dea0cdecbf56f306de7652
Muhammad Juman <muhammad1juman@gmail.com>
2023-04-13 23:06:25 +0500

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Hotel from './pages/hotel/Hotel'
