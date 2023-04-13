aeae36c9b3a6fe2a80ec6c57fff13de1a71b30da
Muhammad Juman <muhammad1juman@gmail.com>
2023-04-13 23:01:03 +0500

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Hotel from './pages/hotel/Hotel'
 
 function App() {
   return (
-    <BrowserRouter basename='/React-Booking-Project'>
+    <BrowserRouter>
       <Routes>
-        <Route path="/React-Booking-Project" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
         <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
